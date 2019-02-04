4e6dd2da9208779516484c4e2fe956ed11748afe
Teletha <Teletha.T@gmail.com>
2019-02-04 15:58:50 +0900

    Provide PathOperational.

---
+++ src/main/java/psychopath/PathOperational.java
@@ -0,0 +1,148 @@
+/*
+ * Copyright (C) 2019 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package psychopath;
+
+import java.util.function.Function;
+
+import kiss.I;
+import kiss.Signal;
+
+public interface PathOperational {
+
+    /**
+     * Copy resources to the destination {@link Directory}.
+     * 
+     * @param patterns A list of glob patterns to accept file by its name.
+     * @return A destination {@link Directory}.
+     */
+    default void deleteNow(String... patterns) {
+        delete(patterns).to(I.NoOP);
+    }
+
+    /**
+     * Build stream that copy resources to the destination {@link Directory}.
+     * 
+     * @param patterns A list of glob patterns to accept file by its name.
+     * @return A event stream which emits operated {@link File}s.
+     */
+    Signal<Location> delete(String... patterns);
+
+    /**
+     * Copy resources to the destination {@link Directory}.
+     * 
+     * @param destination A destination {@link Directory}.
+     * @param patterns A list of glob patterns to accept file by its name.
+     * @return A destination {@link Directory}.
+     */
+    default Directory copyToNow(Directory destination, String... patterns) {
+        return copyToNow(destination, o -> o.glob(patterns));
+    }
+
+    /**
+     * Copy resources to the destination {@link Directory} with some {@link Option}.
+     * 
+     * @param destination A destination {@link Directory}.
+     * @param option A operation {@link Option}.
+     * @return A destination {@link Directory}.
+     */
+    default Directory copyToNow(Directory destination, Function<Option, Option> option) {
+        copyTo(destination, option).to(I.NoOP);
+
+        return destination;
+    }
+
+    /**
+     * Build stream that copy resources to the destination {@link Directory}.
+     * 
+     * @param destination A destination {@link Directory}.
+     * @param patterns A list of glob patterns to accept file by its name.
+     * @return A event stream which emits operated {@link File}s.
+     */
+    default Signal<Location> copyTo(Directory destination, String... patterns) {
+        return copyTo(destination, o -> o.glob(patterns));
+    }
+
+    /**
+     * Build stream that copy resources to the destination {@link Directory} with some
+     * {@link Option}.
+     * 
+     * @param destination A destination {@link Directory}.
+     * @param option A operation {@link Option}.
+     * @return A event stream which emits operated {@link File}s.
+     */
+    Signal<Location> copyTo(Directory destination, Function<Option, Option> option);
+
+    /**
+     * Move resources to the destination {@link Directory}.
+     * 
+     * @param destination A destination {@link Directory}.
+     * @param patterns A list of glob patterns to accept file by its name.
+     * @return A destination {@link Directory}.
+     */
+    default Directory moveToNow(Directory destination, String... patterns) {
+        return moveToNow(destination, o -> o.glob(patterns));
+    }
+
+    /**
+     * Move resources to the destination {@link Directory} with some {@link Option}.
+     * 
+     * @param destination A destination {@link Directory}.
+     * @param option A operation {@link Option}.
+     * @return A destination {@link Directory}.
+     */
+    default Directory moveToNow(Directory destination, Function<Option, Option> option) {
+        moveTo(destination, option).to(I.NoOP);
+
+        return destination;
+    }
+
+    /**
+     * Build stream that move resources to the destination {@link Directory}.
+     * 
+     * @param destination A destination {@link Directory}.
+     * @param patterns A list of glob patterns to accept file by its name.
+     * @return A event stream which emits operated {@link File}s.
+     */
+    default Signal<Location> moveTo(Directory destination, String... patterns) {
+        return moveTo(destination, o -> o.glob(patterns));
+    }
+
+    /**
+     * Build stream that move resources to the destination {@link Directory} with some
+     * {@link Option}.
+     * 
+     * @param destination A destination {@link Directory}.
+     * @param option A operation {@link Option}.
+     * @return A event stream which emits operated {@link File}s.
+     */
+    Signal<Location> moveTo(Directory destination, Function<Option, Option> option);
+
+    /**
+     * Pack resources to the destination {@link File}.
+     * 
+     * @param destination A destination {@link File}.
+     * @param patterns A list of glob patterns to accept file by its name.
+     * @return A destination {@link File}.
+     */
+    default File packToNow(File destination, String... patterns) {
+        packTo(destination, patterns).to(I.NoOP);
+
+        return destination;
+    }
+
+    /**
+     * Build stream that pack resources to the destination {@link File}.
+     * 
+     * @param destination A destination {@link File}.
+     * @param patterns A list of glob patterns to accept file by its name.
+     * @return A event stream which emits operated {@link File}s.
+     */
+    Signal<Location> packTo(File destination, String... patterns);
+}
