731bcca1967aca6e502e3b927ae66ca2c2f4b144
Teletha <Teletha.T@gmail.com>
2020-07-01 12:19:20 +0900

    Version up to 1.0.0

--- src/project/java/Project.java
+++ src/project/java/Project.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 psychopath Development Team
+ * Copyright (C) 2020 psychopath Development Team
  *
  * Licensed under the MIT License (the "License");
  * you may not use this file except in compliance with the License.
@@ -10,10 +10,10 @@
 public class Project extends bee.api.Project {
 
     {
-        product("com.github.teletha", "psychopath", "0.9");
+        product("com.github.teletha", "psychopath", "1.0.0");
 
-        require("com.github.teletha", "sinobu", "LATEST");
-        require("com.github.teletha", "antibug", "LATEST").atTest();
+        require("com.github.teletha", "sinobu", "[2.0,)");
+        require("com.github.teletha", "antibug").atTest();
 
         versionControlSystem("https://github.com/Teletha/Psychopath");
     }
