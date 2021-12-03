beb7ca9c11513e71bbbd5211f9c6cdf00ad4ac67
teletha <teletha@users.noreply.github.com>
2021-12-03 13:59:18 +0000

    deploy: 5d9f41d20b7a55f6ed9983945ff1dd2a8d3c74de

---
+++ root.js
@@ -0,0 +1,58 @@
+const root = {
+	"docs": [],
+	"modules": [],
+	"packages": [
+		"psychopath"
+	],
+	"types": [
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Directory",
+			"packageName": "psychopath",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "File",
+			"packageName": "psychopath",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Folder",
+			"packageName": "psychopath",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Location",
+			"packageName": "psychopath",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Locator",
+			"packageName": "psychopath",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Option",
+			"packageName": "psychopath",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Progress",
+			"packageName": "psychopath",
+			"type": "Class"
+		}
+	]
+}
