aa437e4be901f7f6e7a73d7c9eec3af556b22f39
Teletha <Teletha.T@gmail.com>
2021-12-03 23:54:18 +0900

    update pom

--- bee.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-SET bee="%JAVA_HOME%/lib/bee/bee-0.17.0.jar"
-if not exist %bee% (
-  echo %bee% is not found, try to download it from network.
-  curl -#L -o %bee% https://github.com/teletha/bee/raw/master/bee-0.10.0.jar
-)
-java -javaagent:%bee% -cp %bee% bee.Bee %*
